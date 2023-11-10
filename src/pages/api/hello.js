b2b27e9be1f9f2c07a3e726f0b6863e7e2b6bf48
Maxim Ignatev <ignatif@gmail.com>
2023-11-10 14:13:04 +0400

    init

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Pinecone } from "@pinecone-database/pinecone";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const pinecone = new Pinecone({
+    environment: "gcp-starter",
+    apiKey: "3bacdd76-4962-4cdb-8441-5792a0e2c37e",
+  });
+
+  const index = pinecone.Index("questions");
+  console.log(index);
+  res.status(200).json({ name: "John Doe" });
 }
